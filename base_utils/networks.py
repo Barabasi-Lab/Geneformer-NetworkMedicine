037a44b6dd2fa5741935746afb12553e73f47470
Christian de Frondeville <defrondeville.c@northeastern.edu>
2023-12-13 00:37:14 -0500

    Update networks.py

--- base_utils/networks.py
+++ base_utils/networks.py
@@ -81,10 +81,11 @@ def instantiate_ppi(PPI = Path('/work/ccnr/GeneFormer/GeneFormer_repo/PPI/PPI_20
     # Converts PPI into networkx graph, than trims all nodes that cannot be converted
     PPI = nx.from_pandas_edgelist(PPI)
     PPI = PPI.subgraph(conversion)
-  
-    # Throws out nodes with no embeddings, modules, or edges (disconnected via conversion)
-    nodes_to_keep = [node for node, data in PPI.nodes(data=True) if 'embedding' in list(data.keys())]
-    
+    PPI = nx.Graph(PPI)
+    self_loops = list(nx.selfloop_edges(PPI))
+    PPI.remove_edges_from(self_loops)
+
+
     # Saves PPI if applicable
     if save == True:
         with open('PPI.pk', 'wb') as f:
@@ -145,72 +146,106 @@ def neighbor_group(graph, nodes, LCC = None):
     
 # Compares two generated graphs
 def compare_networks(old_PPI, new_PPI, LCC = None):
-    shared_edges = 0
-    total_edges = 0
-    
-    # Consider bidirectional as unidirectional
-    for edge in old_PPI.edges(data=True):
-        total_edges += 1
-        if new_PPI.has_edge(edge[0], edge[1]) or new_PPI.has_edge(edge[1], edge[0]):
-            shared_edges += 1
-    
-    # Obtains proportion of shared edges
-    proportion_shared = shared_edges / total_edges if total_edges > 0 else 0
-    print(f'Proportion of shared edges: {proportion_shared}')
-    
-    # Obtains modularity of both 
-    new_PPI = new_PPI.to_undirected()
-    partition = community_louvain.best_partition(new_PPI)
-    old_partition = community_louvain.best_partition(old_PPI)
-    modularity = community_louvain.modularity(partition, new_PPI)
-    old_modularity = community_louvain.modularity(old_partition, old_PPI)
-    print(f'New PPI modularity: {modularity} Old PPI modularity: {old_modularity}')
-    
-    # Calculates degree distribution
-    def get_degree_distribution(graph):
-        degrees = [degree for node, degree in nx.degree(graph)]
-        return degrees
-    
-    # Calculate degree distributions
-    old_ppi_degrees = get_degree_distribution(old_PPI)
-    new_ppi_degrees = get_degree_distribution(new_PPI)
-    
-    plt.hist(old_ppi_degrees, bins=max(old_ppi_degrees), alpha=0.5, label='Old PPI')
-    plt.hist(new_ppi_degrees, bins=max(new_ppi_degrees), alpha=0.5, label='New PPI')
-    plt.xlabel('Degree')
-    plt.yscale('log')
-    plt.xscale('log')
-    plt.ylabel('Frequency')
-    plt.title('Degree Distribution of Old PPI vs. New PPI')
-    plt.legend()
-    plt.savefig('NewPPIoldPPIDegreeDisttribution.png')
-    
-    # Calculates second moment
-    def nth_moment_v2(PPI, n):
-        degree_np = np.array(list(dict(PPI.degree).values()))
-        return (sum(degree_np**n)/len(PPI))
-    old_moment = nth_moment_v2(old_PPI, 2)
-    new_moment = nth_moment_v2(new_PPI, 2)
-    print(f'PPI second moment: {old_moment}, GF PPI second moment: {new_moment}')
-
+   
+    def get_log_degree_distribution(graph):
+        # Calculate degree for each node and filter out zero degrees
+        degrees = [degree for node, degree in nx.degree(graph) if degree > 0]
+        # Transform to log-scale; use np.log1p for log(1 + degree) to handle degrees of 1
+        log_degrees = np.log1p(degrees)
+        return log_degrees
+        
     if LCC:
-        # Identify the LCC in the old PPI
-        lcc_nodes = set(max(nx.connected_components(old_PPI), key=len))
-        lcc_edges = old_PPI.subgraph(lcc_nodes).edges()
-
-        # Check node and edge preservation in the new PPI
+        # Assuming LCC is already the largest connected component
+        lcc_nodes = set(LCC.nodes())
+        lcc_edges = set(LCC.edges())
+    
+        # Check node and edge preservation in the new_PPI
         preserved_nodes = lcc_nodes.intersection(set(new_PPI.nodes()))
         preserved_edges = set()
         for edge in lcc_edges:
             if new_PPI.has_edge(*edge) or new_PPI.has_edge(edge[1], edge[0]):
                 preserved_edges.add(edge)
-
+        
         # Calculate the preservation percentages
         node_preservation = len(preserved_nodes) / len(lcc_nodes) if lcc_nodes else 0
         edge_preservation = len(preserved_edges) / len(lcc_edges) if lcc_edges else 0
-
-        print(f'LCC Node Preservation: {node_preservation}')
-        print(f'LCC Edge Preservation: {edge_preservation}')
+        
+        print(f'LCC Node Preservation: {node_preservation:.2%}')
+        print(f'LCC Edge Preservation: {edge_preservation:.2%}')
+        
+        # Calculate and print the edges in the new LCC that are not preserved
+        new_lcc_edges = set(new_PPI.edges())
+        non_preserved_edges = new_lcc_edges.difference(preserved_edges)
+        
+        print("Edges in the new LCC that are not preserved:")
+        for (u, v) in non_preserved_edges:
+            print(f'{u} {v} {new_PPI[u][v]}')
+        
+        # Calculate log-scale degree distributions
+        old_ppi_log_degrees = get_log_degree_distribution(LCC)
+        new_ppi_log_degrees = get_log_degree_distribution(new_PPI)
+        
+        # Plot histograms
+        plt.hist(old_ppi_log_degrees, bins=50, alpha=0.5, label='Old PPI')
+        plt.hist(new_ppi_log_degrees, bins=50, alpha=0.5, label='New PPI')
+        plt.xlabel('Log(Degree)')
+        plt.ylabel('Frequency')
+        plt.xlim(1, None)
+        plt.title('Log-Scale Degree Distribution of Old PPI vs. New PPI')
+        plt.legend()
+        plt.savefig('NewLCColdLCCLogDegreeDisttribution.png')
+        
+        # Calculates second moment
+        def nth_moment_v2(PPI, n):
+            degree_np = np.array(list(dict(PPI.degree).values()))
+            return (sum(degree_np**n)/len(PPI))
+        old_moment = nth_moment_v2(LCC, 2)
+        new_moment = nth_moment_v2(new_PPI, 2)
+        print(f'LCC second moment: {old_moment}, GF LCC second moment: {new_moment}')
+        
+    else:
+        shared_edges = 0
+        total_edges = 0
+        
+        # Consider bidirectional as unidirectional
+        for edge in old_PPI.edges(data=True):
+            total_edges += 1
+            if new_PPI.has_edge(edge[0], edge[1]) or new_PPI.has_edge(edge[1], edge[0]):
+                shared_edges += 1
+        
+        # Obtains proportion of shared edges
+        proportion_shared = shared_edges / total_edges if total_edges > 0 else 0
+        print(f'Proportion of shared edges: {proportion_shared}')
+        
+        # Obtains modularity of both 
+        new_PPI = new_PPI.to_undirected()
+        partition = community_louvain.best_partition(new_PPI)
+        old_partition = community_louvain.best_partition(old_PPI)
+        modularity = community_louvain.modularity(partition, new_PPI)
+        old_modularity = community_louvain.modularity(old_partition, old_PPI)
+        print(f'New PPI modularity: {modularity} Old PPI modularity: {old_modularity}')
+        
+        # Calculate log-scale degree distributions
+        old_ppi_log_degrees = get_log_degree_distribution(old_PPI)
+        new_ppi_log_degrees = get_log_degree_distribution(new_PPI)
+        
+        # Plot histograms
+        plt.hist(old_ppi_log_degrees, bins=50, alpha=0.5, label='Old PPI')
+        plt.hist(new_ppi_log_degrees, bins=50, alpha=0.5, label='New PPI')
+        plt.xlabel('Log(Degree)')
+        plt.ylabel('Frequency')
+        plt.xlim(1, None)
+        plt.title('Log-Scale Degree Distribution of Old PPI vs. New PPI')
+        plt.legend()
+        plt.savefig('NewPPIoldPPILogDegreeDisttribution.png')
+        
+        # Calculates second moment
+        def nth_moment_v2(PPI, n):
+            degree_np = np.array(list(dict(PPI.degree).values()))
+            return (sum(degree_np**n)/len(PPI))
+        old_moment = nth_moment_v2(old_PPI, 2)
+        new_moment = nth_moment_v2(new_PPI, 2)
+        print(f'PPI second moment: {old_moment}, GF PPI second moment: {new_moment}')
 
     
 
