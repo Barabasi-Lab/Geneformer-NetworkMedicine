2efd42e171d24e40e440997773e41e05a78a7435
jspector792 <113631790+jspector792@users.noreply.github.com>
2024-11-07 18:27:38 -0500

    Update figure_3 (1).py

--- code/figure_3 (1).py
+++ code/figure_3 (1).py
@@ -8,20 +8,22 @@ from tqdm import tqdm
 
 import gf_tools as gf
 
-path_to_figs = "/work/ccnr/GeneFormer/aggregation_scripts/plotting_scripts/final_figure_scripts/out/"
+path_to_figs = "path to figure output folder"
 
-PPI = pd.read_csv('/work/ccnr/GeneFormer/jjs_adventures/figure_3_dis_mods/other_data/ppi_with_gf_tokens.csv')
+PPI = pd.read_csv('./supplemental_data/ppi.csv')
 ppi = nx.from_pandas_edgelist(PPI, source = 'ens1', target = 'ens2')
-with open("/work/ccnr/GeneFormer/conda_environment/geneformer/gene_name_id_dict.pkl", 'rb') as f:
-    symbol_to_ensembl = pickle.load(f)
 
-gda = pd.read_csv('/work/ccnr/GeneFormer/jjs_adventures/figure_3_dis_mods/other_data/GDA_Filtered_04042022.csv')
+gda = pd.read_csv('./supplemental_data/gda.csv')
+
+with open("./supplemental_data/gene_name_id_dict.pkl", 'rb') as f:
+    symbol_to_ensembl = pickle.load(f)
+    
 disease_genes = list(gda[gda['NewName'].str.contains("cardiomyopathy dilated")]['HGNC_Symbol'])
 disease_genes_filtered = [symbol_to_ensembl[gene] for gene in disease_genes if gene in symbol_to_ensembl.keys()]
 
-base_path = "/work/ccnr/GeneFormer/aggregated_matrices/"
-mat_paths = ["aggregated_attentions/cardiomyopathy_failing/dilated/fine_tuned/max/layer_4/","aggregated_attentions/cardiomyopathy_failing/dilated/pretrained/max/",
-"aggregated_embeddings/cardiomyopathy_failing/dilated/fine_tuned/max/layer_4/","aggregated_embeddings/cardiomyopathy_failing/dilated/pretrained/max/"]
+base_path = "./data/aggregated_matrices/"
+mat_paths = ["aggregated_attentions/dcm_samples/fine_tuned/max/layer_4/","aggregated_attentions/dcm_samples/pretrained/max/",
+"aggregated_embeddings/dcm_samples/fine_tuned/max/layer_4/","aggregated_embeddings/dcm_samples/pretrained/max/"]
 labels = ["fine-tuned attentions","pretrained attentions", "fine-tuned embeddings", "pretrained embeddings"]
 
 count_dict = {}
